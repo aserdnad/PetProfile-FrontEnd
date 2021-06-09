ede7418848817aa719adf6ea20fc14943adae6ab
Victoria Dezerega <videzere@gmail.com>
2021-06-09 19:41:21 -0400

    Actualización Home v3

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,12 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useRouteMatch, useHistory } from "react-router-dom";
 
 export const Navbar = () => {
+	const match = useRouteMatch();
+	const history = useHistory();
+	useEffect(
+		() => {
+			console.log(match);
+			console.log(history.location.pathname);
+		},
+		[match]
+	);
 	return (
 		<nav className="navbar expand-lg navbar-dark bg-dark m-4">
 			<Link to="/">
 				<p className="navbar-brand m-4 h1">Pet Profile</p>
 			</Link>
+			<div className="ml-auto">
+				{history.location.pathname == "/" && (
+					<Link to="/FormPerson">
+						<button className="btn btn-light p-4">¡Continuemos!</button>
+					</Link>
+				)}
+			</div>
 		</nav>
 	);
 };
