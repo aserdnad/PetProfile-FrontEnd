feed70cd1523d60cd3cac36b5a334b538f41f15a
EuclidesG <euclidesgarrido95@gmail.com>
2021-06-14 18:18:24 -0400

    Falta conectar al backend

--- src/js/views/formperson.js
+++ src/js/views/formperson.js
@@ -1,41 +1,179 @@
+import axios from "axios";
 import React from "react";
+import { useState } from "react";
 
 export function FormPerson() {
+	const [UserName, setUserName] = useState("");
+	const [Name, setName] = useState("");
+	const [Apellido, setApellido] = useState("");
+	const [Contacto, setContacto] = useState("");
+	const [FechaNacimiento, setFechaNacimiento] = useState("");
+	const [Pais, setPais] = useState("");
+	const [Ciudad, setCiudad] = useState("");
+	const [Direccion, setDireccion] = useState("");
+	const [Email, setEmail] = useState("");
+
+	function UserNameHandler(event) {
+		setUserName(event.target.value);
+	}
+
+	function NameHandler(event) {
+		setName(event.target.value);
+	}
+
+	function ApellidoHandler(event) {
+		setApellido(event.target.value);
+	}
+
+	function ApellidoHandler(event) {
+		setApellido(event.target.value);
+	}
+
+	function ContactoHandler(event) {
+		setContacto(event.target.value);
+	}
+
+	function FechaNacimientoHandler(event) {
+		setFechaNacimiento(event.target.value);
+	}
+
+	function PaisHandler(event) {
+		setPais(event.target.value);
+	}
+
+	function CiudadHandler(event) {
+		setCiudad(event.target.value);
+	}
+
+	function DireccionHandler(event) {
+		setDireccion(event.target.value);
+	}
+
+	function EmailHandler(event) {
+		setEmail(event.target.value);
+	}
+
+	function handlerSubmit(event) {
+		const axios = require("axios");
+
+		axios
+			.post("http://localhost:3000/users", {
+				id: 6,
+				first_name: "Fred",
+				last_name: "Blair",
+				email: "freddyb34@gmail.com"
+			})
+			.then(resp => {
+				console.log(resp.data);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+		/*
+		axios.post("http://localhost:3000/users"),
+			{
+				UserName,
+				Name,
+				Apellido,
+				Contacto,
+				FechaNacimiento,
+				Pais,
+				Ciudad,
+				Direccion,
+				Email
+			}.then(response => {
+				if (response.ok) {
+					console.log(response.data);
+					console.log("Informacion enviada!");
+				} else {
+					console.log("Informacion NO enviada :(");
+				}
+		*	});*/
+	}
+
 	return (
 		<form className="form-format">
 			<h1>Datos personales</h1>
 			<hr className="style2" />
 			<div className="form-group">
-				<label HtmlFor="formGroupExampleInput">Username</label>
-				<input type="text" className="form-control" id="formGroupExampleInput" placeholder="" />
+				<label htmlFor="formGroupExampleInput">Username</label>
+				<input
+					type="text"
+					className="form-control"
+					id="formGroupExampleInput"
+					placeholder=""
+					onChange={UserNameHandler}
+				/>
 			</div>
 			<div className="form-group">
-				<label HtmlFor="formGroupExampleInput">Nombre</label>
-				<input type="text" className="form-control" id="formGroupExampleInput" placeholder="" />
+				<label htmlFor="formGroupExampleInput">Nombre</label>
+				<input
+					type="text"
+					className="form-control"
+					id="formGroupExampleInput"
+					placeholder=""
+					onChange={NameHandler}
+				/>
 			</div>
 			<div className="form-group">
-				<label HtmlFor="formGroupExampleInput">Apellido</label>
-				<input type="text" className="form-control" id="formGroupExampleInput" placeholder="" />
+				<label htmlFor="formGroupExampleInput">Apellido</label>
+				<input
+					type="text"
+					className="form-control"
+					id="formGroupExampleInput"
+					placeholder=""
+					onChange={ApellidoHandler}
+				/>
 			</div>
 			<div className="form-group">
-				<label HtmlFor="formGroupExampleInput">Número de contacto</label>
-				<input type="text" className="form-control" id="formGroupExampleInput" placeholder="" />
+				<label htmlFor="formGroupExampleInput">Contacto</label>
+				<input
+					type="text"
+					className="form-control"
+					id="formGroupExampleInput"
+					placeholder=""
+					onChange={ContactoHandler}
+				/>
 			</div>
 			<div className="form-group">
-				<label HtmlFor="formGroupExampleInput">Cumpleaños</label>
-				<input type="text" className="form-control" id="formGroupExampleInput" placeholder="" />
+				<label htmlFor="formGroupExampleInput">Cumpleaños</label>
+				<input
+					type="date"
+					className="form-control"
+					id="formGroupExampleInput"
+					placeholder=""
+					onChange={FechaNacimientoHandler}
+				/>
 			</div>
 			<div className="form-group">
-				<label HtmlFor="formGroupExampleInput">País</label>
-				<input type="text" className="form-control" id="formGroupExampleInput" placeholder="" />
+				<label htmlFor="formGroupExampleInput">País</label>
+				<input
+					type="text"
+					className="form-control"
+					id="formGroupExampleInput"
+					placeholder=""
+					onChange={PaisHandler}
+				/>
 			</div>
 			<div className="form-group">
-				<label HtmlFor="formGroupExampleInput">Ciudad</label>
-				<input type="text" className="form-control" id="formGroupExampleInput" placeholder="" />
+				<label htmlFor="formGroupExampleInput">Ciudad</label>
+				<input
+					type="text"
+					className="form-control"
+					id="formGroupExampleInput"
+					placeholder=""
+					onChange={CiudadHandler}
+				/>
 			</div>
 			<div className="form-group">
-				<label HtmlFor="formGroupExampleInput">Dirección</label>
-				<input type="text" className="form-control" id="formGroupExampleInput" placeholder="" />
+				<label htmlFor="formGroupExampleInput">Dirección</label>
+				<input
+					type="text"
+					className="form-control"
+					id="formGroupExampleInput"
+					placeholder=""
+					onChange={DireccionHandler}
+				/>
 			</div>
 			<h1>Datos de la cuenta</h1>
 			<hr className="style2" />
@@ -47,6 +185,7 @@ export function FormPerson() {
 					id="exampleInputEmail1"
 					aria-describedby="emailHelp"
 					placeholder="Introduce correo electronico"
+					onChange={EmailHandler}
 				/>
 			</div>
 			<div className="form-group">
@@ -59,7 +198,7 @@ export function FormPerson() {
 
 				<input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
 			</div>
-			<button type="submit" className="btn btn-success">
+			<button type="submit" className="btn btn-success" onClick={handlerSubmit}>
 				Registrarse
 			</button>
 		</form>
