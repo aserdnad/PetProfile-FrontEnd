c93ea55e62078b010963dd9e49c852753f744ff0
Victoria Dezerega <79772939+videzere@users.noreply.github.com>
2021-06-14 21:14:08 -0400

    Botones registro e inicio de sesión - navbar home

---
+++ navbar.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { Link, useRouteMatch, useHistory } from "react-router-dom";
+import logo from "../../img/logo.png";
+
+export const Navbar = () => {
+	const match = useRouteMatch();
+	const history = useHistory();
+	const handleClick1 = () => history.push("/LogIn");
+	const handleClick2 = () => history.push("/FormPerson");
+	useEffect(
+		() => {
+			console.log(match);
+			console.log(history.location.pathname);
+		},
+		[match]
+	);
+	return (
+		<nav className="navbar expand-lg navbar-dark bg-dark m-4">
+			<Link to="/">
+				<img className="w-25" src={logo} />
+			</Link>
+			<div className="ml-auto">
+				{history.location.pathname == "/" && (
+					<Link>
+						<button className="btn btn-light m-1 p-3" type="button" onClick={handleClick1}>
+							¡Regístrate!
+						</button>
+						<button className="btn btn-light m-1 p-3" type="button" onClick={handleClick2}>
+							¡Inicia Sesión!
+						</button>
+					</Link>
+				)}
+			</div>
+		</nav>
+	);
+};
