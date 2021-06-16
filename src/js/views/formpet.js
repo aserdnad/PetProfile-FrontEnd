dabb8df919368342547a207f5045b78fbfa18a3c
Victoria Dezerega <videzere@gmail.com>
2021-06-16 17:40:54 -0400

    Fetch no sirve, comentado

--- src/js/views/formpet.js
+++ src/js/views/formpet.js
@@ -4,16 +4,19 @@ import { Form } from "react-bootstrap";
 
 export function FormPet() {
 	const [Name, setName] = useState("");
+	const [Species, setSpecies] = useState("");
 	const [Race, setRace] = useState("");
 	const [Gender, setGender] = useState("");
 	const [Birthday, setBirthday] = useState("");
 	const [Age, setAge] = useState("");
-	const [Species, setSpecies] = useState("");
 	const [Weight, setWeight] = useState("");
 	const [Height, setHeight] = useState("");
 
 	function NameHandler(event) {
 		setName(event.target.value);
+	}
+	function SpeciesHandler(event) {
+		setSpecies(event.target.value);
 	}
 	function RaceHandler(event) {
 		setRace(event.target.value);
@@ -27,9 +30,6 @@ export function FormPet() {
 	function AgeHandler(event) {
 		setAge(event.target.value);
 	}
-	function SpeciesHandler(event) {
-		setSpecies(event.target.value);
-	}
 	function WeightHandler(event) {
 		setWeight(event.target.value);
 	}
@@ -37,7 +37,42 @@ export function FormPet() {
 		setHeight(event.target.value);
 	}
 	function handlerSubmit(event) {}
-
+	// function handlerSubmit(event) = async () => {
+	//     try {
+	//         let resp = await fetch(
+	//             `https://gitpod.io/#https://github.com/aserdnad/PetProfile-BackEnd`,
+	//             {
+	//                 method: "POST",
+	//                 headers: {
+	//                     "Content-Type": "application/json"
+	//                 },
+	//                 body: JSON.stringify([
+	//                     Name,
+	//                     Species,
+	//                     Race,
+	//                     Gender,
+	//                     Birthday,
+	//                     Age,
+	//                     Weight,
+	//                     Height
+	//                 ])
+	//             }
+	//         );
+	//             let data = await resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+	//             if (resp.ok) {
+	//                 console.log(resp.ok); // will be true if the response is successfull
+	//                 console.log(resp.status); // the status code = 200 or code = 400 etc.
+	//                 console.log(resp.text()); // will try return the exact result as string			//here is were your code should start after the fetch finishes
+	//                 console.log(data); //this will print on the console the exact object received from the server
+	//             } else {
+	//                 console.log(resp.status); // the status code = 200 or code = 400 etc.
+	//                 console.log(data); //this will print on the console the exact object received from the server
+	//             }
+	//         } catch (error) {
+	//             //error handling
+	//             console.log(error);
+	//         }
+	//     };
 	return (
 		<Form className="form-format">
 			<h1>Datos de la Mascota</h1>
