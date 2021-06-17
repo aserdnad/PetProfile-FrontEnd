14878a8cfd0732595bfd349b38a1b9a39124b430
aserdnad <andreasgamardo@gmail.com>
2021-06-17 19:37:40 +0200

    comienzo calendario aiuda

---
+++ src/js/views/calendario.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../styles/home.scss";
+// import { Row, Col, Jumbotron, Button, Card, Form } from "react-bootstrap";
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
+
+export const Calendario = () => {
+	return (
+		<h1>hola</h1>
+	);
+};
